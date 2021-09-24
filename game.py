22ae0a507db79b0cde28a0459f7c0b88305f2016
maahirajw <89554237+maahirajw@users.noreply.github.com>
2021-09-24 14:39:17 -0400

    Update game.py

--- game.py
+++ game.py
@@ -10,7 +10,10 @@ x = os.getenv("PLAYER_NAME")
 import random
 options = ["rock", "paper", "scissors"]
 
+print("Welcome", x, "to my rock-paper-scissors game!")
+print ("-------------------")
 print("Rock, Paper, Scissors, Shoot!")
+
 
 #PROMPT USER FOR INPUT 
 
@@ -26,30 +29,43 @@ options = ["rock", "paper", "scissors"]
 computer_choice = random.choice(options)
 print ("computer chose:")
 print (computer_choice)
+print ("-------------------")
 
 if (user_choice == 'rock' and computer_choice == 'paper'): 
     print ("oh, the computer won. It's ok")
-elif user_choice == 'paper' and 'computer_choice' == 'scissors': 
+    print ("-------------------")
+elif user_choice == 'paper' and computer_choice == 'scissors': 
     print ("oh, the computer won. It's ok")
+    print ("-------------------")
 elif user_choice == 'scissors' and computer_choice == 'rock':
     print ("oh, the computer won. It's ok")
+    print ("-------------------")
 elif user_choice == 'rock' and computer_choice == 'scissors':
     print ("You won. Congratulations!")
+    print ("-------------------")
 elif user_choice == 'paper' and computer_choice == 'rock': 
     print ("You won. Congratulations!")
+    print ("-------------------")
 elif user_choice == 'scissors' and computer_choice == 'paper': 
     print ("You won. Congratulations!")
+    print ("-------------------")
 elif user_choice == 'rock' and computer_choice == 'rock':
     print ("It is a tie.")
+    print ("-------------------")
 elif user_choice == 'paper' and computer_choice == 'paper': 
     print ("It is a tie.")
+    print ("-------------------")
 elif user_choice == 'scissors' and computer_choice == 'scissors': 
     print ("It is a tie.")
+    print ("-------------------")
+else:
+    print ("Your choice is invalid.")
+    print ("Be sure to check that all your letters are in lowercase!")
+    print ("-------------------")
 
 
-print("THANKS PLEASE PLAY AGAIN")
+print("Thanks for playing.Hope to see you again!")
+print ("-------------------")
+
 
 #breakpoint()
-
-
-
